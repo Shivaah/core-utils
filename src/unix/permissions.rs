955fab8fc97e4fb2a73b8317b7cd9cf8ed689b93
Adrien Shivz <seikay83@gmail.com>
2023-10-07 22:16:09 +0200

    feat: add some utilies for permissions
    - impl Display trait for Permission
    - add PermissionFlag struct and impl BitAnd trait for u32

--- src/unix/permissions.rs
+++ src/unix/permissions.rs
@@ -1,18 +1,44 @@
-use std::os::unix::fs::PermissionsExt;
+use std::{ops::BitAnd, os::unix::fs::PermissionsExt};
+
+enum PermissionFlag {
+    READ = 0o4,
+    WRITE = 0o2,
+    EXECUTE = 0o1,
+}
+
+impl BitAnd<PermissionFlag> for u32 {
+    type Output = u32;
+
+    fn bitand(self, rhs: PermissionFlag) -> Self::Output {
+        self & rhs as u32
+    }
+}
 
 pub struct Permission(u32);
 
 impl Permission {
-    fn readable(&self) -> bool {
-        self.0 & 0o4 > 0
+    pub fn readable(&self) -> bool {
+        self.0 & PermissionFlag::READ > 0
     }
 
-    fn writable(self) -> bool {
-        self.0 & 0o2 > 0
+    pub fn writable(&self) -> bool {
+        self.0 & PermissionFlag::WRITE > 0
     }
 
-    fn executable(self) -> bool {
-        self.0 & 0o1 > 0
+    pub fn executable(&self) -> bool {
+        self.0 & PermissionFlag::EXECUTE > 0
+    }
+}
+
+impl std::fmt::Display for Permission {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}{}",
+            if self.readable() { 'r' } else { '-' },
+            if self.writable() { 'w' } else { '-' },
+            if self.executable() { 'x' } else { '-' }
+        )
     }
 }
 
